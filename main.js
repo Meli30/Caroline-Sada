4be82ca4cbf9f2f3b202b2c94ae639b1971d94b0
melisa <melif6108@gmail.com>
2023-12-26 18:33:53 -0300

    carosada

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/*validacion del formulario*/
+const nombre = document.getElementById("name")
+const surname = document.getElementById("surname")
+const email = document.getElementById("email")
+const commment = document.getElementById("comment")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    if (nombre.ariaValueMax.length <8){
+        warnings += `El nombre no es valido`
+        
+    }
+})
+
+
+/*boton de ir arriba*/
 window.onscroll = function(){
 
 if(document.documentElement.scrollTop > 100){
